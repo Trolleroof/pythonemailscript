1ad272c943a2fd0197a541ad9725b418b849330c
Trolleroof <nikhilprabhu06@gmail.com>
2023-03-18 22:35:11 -0700

    added things

---
+++ main.py
@@ -0,0 +1,9 @@
+import csv
+import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email import encoders
+
+email_sender = "nikhilprabhu06@gmail.com"
+password = ""
+subject = "Interest in Research Opportunity"
